00260dbd1259aafb4bd5b93daa5a564451247476
unknown <marcisouckova@gmail.com>
2025-05-15 19:44:16 +0200

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 // next.config.ts
+import path from "path";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  webpack(config) {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname),
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
